150c246b06a1c13804bf3c8ac76a4cce19b9dd28
Nguyen Van Giang <giangnv.22bi13132@usth.edu.vn>
2024-09-09 16:55:21 +0700

    completed first login layout

---
+++ app/src/main/java/vn/edu/usth/wordpressclient/FirstLoginActivity.java
@@ -0,0 +1,17 @@
+package vn.edu.usth.wordpressclient;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_first_login);
+
+    }
+}
