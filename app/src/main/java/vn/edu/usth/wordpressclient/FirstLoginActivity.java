e14c983404e99634388ed786206c057973f09de6
le chuck <hoanghl.22bi13170@usth.edu.vn>
2024-09-10 04:50:29 +0700

    User Web page update

--- app/src/main/java/vn/edu/usth/wordpressclient/FirstLoginActivity.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/FirstLoginActivity.java
@@ -1,17 +1,31 @@
 package vn.edu.usth.wordpressclient;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstLoginActivity extends AppCompatActivity {
 
+    TextView comment_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_first_login);
+        setContentView(R.layout.activity_user_web_mangement);
 
-    }
+        comment_btn = findViewById(R.id.comments_btn);
+
+        comment_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show a toast message or handle the click event here
+                Toast.makeText(FirstLoginActivity.this, "Comment button clicked!", Toast.LENGTH_SHORT).show();
+            };
+        });
+    };
 }
