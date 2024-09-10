fe8cf1d654256e08e0166c4a8819cf5d19a1d6a1
Nguyen Van Giang <giangnv.22bi13132@usth.edu.vn>
2024-09-11 00:23:14 +0700

    Completed posts activity with fragments layout

---
+++ app/src/main/java/vn/edu/usth/wordpressclient/PagesFragments/PageScheduledFragment.java
@@ -0,0 +1,21 @@
+package vn.edu.usth.wordpressclient.PagesFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import vn.edu.usth.wordpressclient.R;
+
+public class PageScheduledFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page_scheduled, container, false);
+    }
+}
