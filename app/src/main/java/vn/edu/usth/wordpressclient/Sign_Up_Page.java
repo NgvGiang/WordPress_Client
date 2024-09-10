8668aca2f8ca048122af99fa94f309d9d079b7ff
le chuck <hoanghl.22bi13170@usth.edu.vn>
2024-09-10 22:48:16 +0700

    Sign up page update

---
+++ app/src/main/java/vn/edu/usth/wordpressclient/Sign_Up_Page.java
@@ -0,0 +1,76 @@
+package vn.edu.usth.wordpressclient;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+public class Sign_Up_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up_page);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        RelativeLayout google_btn = findViewById(R.id.google_button);
+        Button continue_btn = findViewById(R.id.continue_btn);
+
+        // Set the title
+        getSupportActionBar().setTitle("Get started");
+        //title color
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
+        //back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.getNavigationIcon().setTint(ContextCompat.getColor(this, android.R.color.white));
+        toolbar.setNavigationOnClickListener(v -> {
+            finish();
+        });
+        continue_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Continue button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        google_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Google button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    // Inflate the menu for the info icon
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu); // Define a menu for the info icon
+        return true;
+    }
+    // Handle menu item clicks (for the info icon)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.info_icon) {
+            // Handle info icon click
+            Toast.makeText(this, "Info clicked!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
