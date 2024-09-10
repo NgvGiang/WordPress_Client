d0ebbcd8228045ac1f0267892ea7daa0cbf88b69
Mithrandir2002 <thanhthuy.pthn@gmail.com>
2024-09-10 20:17:07 +0700

    Change minSDK from 27 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "vn.edu.usth.wordpressclient"
-        minSdk = 27
+        minSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
