14cf91721bb008dce673798587c4e14d6c7e0b5b
luuduc2727 <trumsnap@gmail.com>
2024-09-11 13:52:58 +0700

    update ver2

--- app/src/main/java/vn/edu/usth/wordpressclient/WordPress_media.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/WordPress_media.java
@@ -4,30 +4,43 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class WordPress_media extends AppCompatActivity {
 
-    private TabLayout mTablayout;
-    private ViewPager mViewPager;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_press_media);
 
-        mTablayout = findViewById(R.id.tablayout);
-        mViewPager = findViewById(R.id.view_pager);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("WordPress Media");
 
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        mViewPager.setAdapter(viewPagerAdapter);
 
-        mTablayout.setupWithViewPager(mViewPager);
+        tabLayout = findViewById(R.id.PagetabLayout);
+        viewPager2 = findViewById(R.id.PageviewPager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPagerAdapter.addFragment(new All_Fragment(), "All");
+        viewPagerAdapter.addFragment(new images_Fragment(), "Images");
+        viewPagerAdapter.addFragment(new Documents_Fragment(), "Documents");
+        viewPagerAdapter.addFragment(new Videos_Fragment(), "Videos");
+        viewPager2.setAdapter(viewPagerAdapter);
+        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
+            tab.setText(viewPagerAdapter.getTitle(position));
+        }).attach();
     }
 }
