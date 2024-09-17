9d8b15700a146ad7f43bd56e02405ee90e2c41cb
DarkHent <154747418+DarkHent1808@users.noreply.github.com>
2024-09-17 15:26:56 +0700

    Updating Dark mode and intent java

--- app/src/main/java/vn/edu/usth/wordpressclient/sign_up_choose_display_name.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/sign_up_choose_display_name.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.wordpressclient;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,12 @@ public class sign_up_choose_display_name extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up_choose_display_name);
+
+        Button create_account = findViewById(R.id.doneButton);
+
+        create_account.setOnClickListener(view -> startActivity(new Intent(this, Welcome_to_WordPress.class)));
+
+
 
     }
 }
