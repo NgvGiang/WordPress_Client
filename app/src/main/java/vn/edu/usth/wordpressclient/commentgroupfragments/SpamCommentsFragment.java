403a9cdd7634b96e1475f45cdc570a197c2e8db5
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2024-10-14 19:58:38 +0700

    handle data from API call in SpamCommentsFragment.java and display them fragment layout

--- app/src/main/java/vn/edu/usth/wordpressclient/commentgroupfragments/SpamCommentsFragment.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/commentgroupfragments/SpamCommentsFragment.java
@@ -2,19 +2,120 @@ package vn.edu.usth.wordpressclient.commentgroupfragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.edu.usth.wordpressclient.CommentRecyclerViewAdapter;
 import vn.edu.usth.wordpressclient.R;
+import vn.edu.usth.wordpressclient.models.Comment;
+import vn.edu.usth.wordpressclient.models.CommentAPIServices;
+import vn.edu.usth.wordpressclient.models.CommentsCallback;
 
 public class SpamCommentsFragment extends Fragment {
+    String userDomain;
+    List<Comment> comments;
+    RecyclerView recyclerView;
+    CommentRecyclerViewAdapter commentRecyclerViewAdapter;
+    private static final int PER_PAGE = 5;
+    private int currentPage = 1;
+    private boolean isLastPage = false;
+    private boolean isLoading = false;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spam_comments, container, false);
+        View view = inflater.inflate(R.layout.fragment_spam_comments, container, false);
+
+        if (savedInstanceState == null) {
+            comments = new ArrayList<>();
+        } else {
+            comments = savedInstanceState.getParcelableArrayList("comments");
+        }
+        recyclerView = view.findViewById(R.id.fragment_spam_comments_rec_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                if (linearLayoutManager != null && linearLayoutManager.findLastVisibleItemPosition() == comments.size() - 1) {
+                    if (!isLoading && !isLastPage) {
+                        loadMoreComments();
+                    }
+                }
+            }
+        });
+        commentRecyclerViewAdapter = new CommentRecyclerViewAdapter(comments, getContext());
+        recyclerView.setAdapter(commentRecyclerViewAdapter);
+        if (savedInstanceState != null) {
+            comments = savedInstanceState.getParcelableArrayList("comments");
+            currentPage = savedInstanceState.getInt("currentPage");
+            isLastPage = savedInstanceState.getBoolean("isLastPage");
+            commentRecyclerViewAdapter.notifyDataSetChanged();
+        } else {
+            loadInitialComments();
+        }
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList("comments", new ArrayList<>(comments));
+        outState.putInt("currentPage", currentPage);
+        outState.putBoolean("isLastPage", isLastPage);
+    }
+
+    private void loadMoreComments() {
+        isLoading = true;
+        CommentAPIServices.getSpamCommentFromUser(getContext(), "peppermint777.wordpress.com", PER_PAGE, currentPage, new CommentsCallback() {
+            @Override
+            public void onSuccess(List<Comment> newComments) {
+                comments.addAll(newComments);
+                commentRecyclerViewAdapter.notifyDataSetChanged();
+                isLoading = false;
+                if (newComments.size() < 5) {
+                    isLastPage = true;
+                } else {
+                    currentPage++;
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                isLoading = false;
+            }
+        });
+    }
+
+    private void loadInitialComments() {
+        isLoading = true;
+        CommentAPIServices.getSpamCommentFromUser(getContext(), "peppermint777.wordpress.com", PER_PAGE, currentPage, new CommentsCallback() {
+            @Override
+            public void onSuccess(List<Comment> newComments) {
+                comments.addAll(newComments);
+                commentRecyclerViewAdapter.notifyDataSetChanged();
+                isLoading = false;
+                if (newComments.size() < 5) {
+                    isLastPage = true;
+                } else {
+                    currentPage++;
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                isLoading = false;
+            }
+        });
     }
 }
