abed8c22874659d01c2c5b650cb0a562a761bd6f
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2024-10-14 20:07:09 +0700

    craete  new CommentDetails Activity

--- app/src/main/java/vn/edu/usth/wordpressclient/CommentDetails.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/CommentDetails.java
@@ -15,10 +15,6 @@ public class CommentDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_comment_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
--- app/src/main/java/vn/edu/usth/wordpressclient/models/CommentsCallback.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/models/CommentsCallback.java
@@ -1,2 +1,8 @@
-package vn.edu.usth.wordpressclient.models;public interface CommentsCallback {
+package vn.edu.usth.wordpressclient.models;
+
+import java.util.List;
+
+public interface CommentsCallback {
+    void onSuccess(List<Comment> newComments);
+    void onError(String error);
 }
