62620d601f916e391d62b9d1caddc86d761f951a
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2024-10-15 12:54:46 +0700

    Round the author avatar and display it using Picasso

--- app/src/main/java/vn/edu/usth/wordpressclient/CommentRecyclerViewAdapter.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/CommentRecyclerViewAdapter.java
@@ -2,10 +2,17 @@ package vn.edu.usth.wordpressclient;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.os.Parcelable;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,6 +20,10 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Period;
@@ -49,11 +60,41 @@ public class CommentRecyclerViewAdapter extends RecyclerView.Adapter<CommentView
         }
         holder.authorNameAndPostName.setText(comments.get(position).getAuthorName() + " on Saturday, 12 October 2024");
         holder.content.setText(Html.fromHtml(comments.get(position).getContent(), Html.FROM_HTML_MODE_LEGACY).toString());
+        Picasso.get().load(comments.get(position).getAuthorAvatar()).error(R.drawable.blank_avatar).transform(new Transformation() {
+            @Override
+            public Bitmap transform(Bitmap source) {
+                int size = Math.min(source.getWidth(), source.getHeight());
+                int x = (source.getWidth() - size) / 2;
+                int y = (source.getHeight() - size) / 2;
+
+                Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+                if (squaredBitmap != source) {
+                    source.recycle();
+                }
+
+                Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+                Canvas canvas = new Canvas(bitmap);
+                Paint paint = new Paint();
+                BitmapShader shader = new BitmapShader(squaredBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+                paint.setShader(shader);
+                paint.setAntiAlias(true);
+                float r = size / 2f;
+                canvas.drawCircle(r, r, r, paint);
+
+                squaredBitmap.recycle();
+                return bitmap;
+            }
+
+            @Override
+            public String key() {
+                return "circle";
+            }
+        }).into(holder.authorAvatar);
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, CommentDetails.class);
-                intent.putExtra("comment", comments.get(holder.getAdapterPosition()));
+                intent.putExtra("comment", (Serializable) comments.get(holder.getAdapterPosition()));
                 context.startActivity(intent);
             }
         });
@@ -67,6 +108,7 @@ public class CommentRecyclerViewAdapter extends RecyclerView.Adapter<CommentView
 
 class CommentViewHolder extends RecyclerView.ViewHolder{
     TextView authorNameAndPostName, content, date;
+    ImageView authorAvatar;
     LinearLayout cardView;
 
     public CommentViewHolder(@NonNull View itemView) {
@@ -74,6 +116,7 @@ class CommentViewHolder extends RecyclerView.ViewHolder{
         authorNameAndPostName = itemView.findViewById(R.id.author_name_comment_and_post_name);
         content = itemView.findViewById(R.id.comment_content);
         date = itemView.findViewById(R.id.comment_date);
+        authorAvatar = itemView.findViewById(R.id.author_avatar_comment);
         cardView = itemView.findViewById(R.id.cmt_rec_card);
     }
 }
