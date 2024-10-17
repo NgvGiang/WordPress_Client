dc965497e8e79345fb48c68ca4d62bfcc1aaa39a
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2024-10-17 14:23:10 +0700

    Test ActivityResult Launcher

--- app/src/main/java/vn/edu/usth/wordpressclient/CommentActivity.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/CommentActivity.java
@@ -1,6 +1,12 @@
 package vn.edu.usth.wordpressclient;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
@@ -16,16 +22,48 @@ import vn.edu.usth.wordpressclient.commentgroupfragments.PendingCommentsFragment
 import vn.edu.usth.wordpressclient.commentgroupfragments.SpamCommentsFragment;
 import vn.edu.usth.wordpressclient.commentgroupfragments.TrashedCommentsFragment;
 import vn.edu.usth.wordpressclient.commentgroupfragments.UnrepliedCommentsFragment;
+import vn.edu.usth.wordpressclient.models.Comment;
 
 public class CommentActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
+    String domain;
+    ActivityResultLauncher<Intent> commentDetailLauncher;
+    AllCommentsFragment allCommentsFragment;
+    PendingCommentsFragment pendingCommentsFragment;
+    UnrepliedCommentsFragment unrepliedCommentsFragment;
+    ApprovedCommentsFragment approvedCommentsFragment;
+    SpamCommentsFragment spamCommentsFragment;
+    TrashedCommentsFragment trashedCommentsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);;
         EdgeToEdge.enable(this);
+
+        Intent intent = getIntent();
+        domain = intent.getStringExtra("domain");
+        Bundle bundle = new Bundle();
+        bundle.putString("domain", domain);
+
+        allCommentsFragment = new AllCommentsFragment();
+        allCommentsFragment.setArguments(bundle);
+
+        pendingCommentsFragment = new PendingCommentsFragment();
+        pendingCommentsFragment.setArguments(bundle);
+
+        unrepliedCommentsFragment = new UnrepliedCommentsFragment();
+        unrepliedCommentsFragment.setArguments(bundle);
+
+        approvedCommentsFragment = new ApprovedCommentsFragment();
+        approvedCommentsFragment.setArguments(bundle);
+
+        spamCommentsFragment = new SpamCommentsFragment();
+        spamCommentsFragment.setArguments(bundle);
+
+        trashedCommentsFragment = new TrashedCommentsFragment();
+        trashedCommentsFragment.setArguments(bundle);
 
         Toolbar toolbar = findViewById(R.id.comment_tool_bar);
         setSupportActionBar(toolbar);
@@ -39,15 +77,46 @@ public class CommentActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.comment_tab_mode);
         viewPager2 = findViewById(R.id.comment_view_pager);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
-        viewPagerAdapter.addFragment(new AllCommentsFragment(), getString(R.string.ALL));
-        viewPagerAdapter.addFragment(new PendingCommentsFragment(), getString(R.string.pending));
-        viewPagerAdapter.addFragment(new UnrepliedCommentsFragment(), getString(R.string.unreplied));
-        viewPagerAdapter.addFragment(new ApprovedCommentsFragment(), getString(R.string.approved));
-        viewPagerAdapter.addFragment(new SpamCommentsFragment(), getString(R.string.spam));
-        viewPagerAdapter.addFragment(new TrashedCommentsFragment(), getString(R.string.trashed));
+        viewPagerAdapter.addFragment(allCommentsFragment, getString(R.string.ALL));
+        viewPagerAdapter.addFragment(pendingCommentsFragment, getString(R.string.pending));
+        viewPagerAdapter.addFragment(unrepliedCommentsFragment, getString(R.string.unreplied));
+        viewPagerAdapter.addFragment(approvedCommentsFragment, getString(R.string.approved));
+        viewPagerAdapter.addFragment(spamCommentsFragment, getString(R.string.spam));
+        viewPagerAdapter.addFragment(trashedCommentsFragment, getString(R.string.trashed));
         viewPager2.setAdapter(viewPagerAdapter);
         new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
             tab.setText(viewPagerAdapter.getTitle(position));
         }).attach();
+
+        commentDetailLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            int position = data.getIntExtra("position", -1);
+                            String newStatus = data.getStringExtra("status");
+
+                            Log.i("position cmt activity", "" + position);
+                            Log.i("status", newStatus);
+                            if (position != -1) {
+                                updateCommentAtPosition(position, newStatus);
+                            }
+                        }
+                    }
+                }
+        );
+    }
+    private void updateCommentAtPosition(int position, String newStatus) {
+        // Update the comment in the corresponding fragment
+        if (newStatus.equals("hold")) {
+            Log.i("update pending", "udp");
+            approvedCommentsFragment.removeCommentAtPosition(position);
+            pendingCommentsFragment.addComment(approvedCommentsFragment.getComments().get(position));
+        } else if (newStatus.equals("approved")) {
+            Log.i("update approved", "uda");
+            pendingCommentsFragment.removeCommentAtPosition(position);
+            approvedCommentsFragment.addComment(spamCommentsFragment.getComments().get(position));
+        }
     }
 }
---
+++ app/src/main/java/vn/edu/usth/wordpressclient/models/CommentDetailCallback.java
@@ -0,0 +1,2 @@
+package vn.edu.usth.wordpressclient.models;public interface CommentDetailCallback {
+}
