0487109a3a08f30a6c4ae0f171ec15daf48622d4
lechuckkfigatos <hoanghl.22bi13170@usth.edu.vn>
2024-10-26 00:14:24 +0700

    Added content cardview xml

--- app/src/main/java/vn/edu/usth/wordpressclient/MeActivity/MeWebsiteActivity.java
+++ app/src/main/java/vn/edu/usth/wordpressclient/MeActivity/MeWebsiteActivity.java
@@ -3,24 +3,48 @@ package vn.edu.usth.wordpressclient.MeActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.Request;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import vn.edu.usth.wordpressclient.R;
 import vn.edu.usth.wordpressclient.SessionManager;
 
+import vn.edu.usth.wordpressclient.SessionManager;
+import vn.edu.usth.wordpressclient.models.QueueManager;
+
 public class MeWebsiteActivity extends AppCompatActivity {
-    SessionManager session;
+    LinearLayout username_btn, profile_btn, account_settings_btn, app_settings_btn,
+            help_btn, share_wordpress_btn, wp_admin_btn, log_out_btn;
+    TextView displayName, accountName;
+    ImageView avatar;
+    private SessionManager session;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me_website);
         EdgeToEdge.enable(this);
-//        SessionManager sessionManagement = new SessionManager(this);
+
         session = SessionManager.getInstance(this);
         LinearLayout username_btn = findViewById(R.id.user_name_btn);
         LinearLayout profile_btn = findViewById(R.id.my_profile);
@@ -31,13 +55,33 @@ public class MeWebsiteActivity extends AppCompatActivity {
         LinearLayout wp_admin_btn = findViewById(R.id.wp_admin);
         LinearLayout log_out_btn = findViewById(R.id.log_out);
 
-        username_btn.setOnClickListener(view -> startActivity(new Intent(this, UsernameActivity.class)));
-        profile_btn.setOnClickListener(view -> startActivity(new Intent(this, MyProfileActivity.class)));
+        session = SessionManager.getInstance(this);
+        String accessToken = session.getAccessToken();
+
+        profile_btn = findViewById(R.id.my_profile);
+        account_settings_btn = findViewById(R.id.account_settings);
+        app_settings_btn = findViewById(R.id.app_settings);
+        help_btn = findViewById(R.id.help);
+        share_wordpress_btn = findViewById(R.id.share_wordpress);
+        wp_admin_btn = findViewById(R.id.wp_admin);
+        log_out_btn = findViewById(R.id.log_out);
+
+        displayName = findViewById(R.id.user_name);
+        accountName = findViewById(R.id.account_name);
+        avatar = findViewById(R.id.ic_profile_placeholder);
+
+        profile_btn.setOnClickListener(view -> {
+            Intent myProfileIntent = new Intent(MeWebsiteActivity.this, MyProfileActivity.class);
+            startActivity(myProfileIntent);
+        });
+        //startActivity(new Intent(this, MyProfileActivity.class)));
         account_settings_btn.setOnClickListener(view -> startActivity(new Intent(this, AccountSettingsActivity.class)));
         app_settings_btn.setOnClickListener(view -> startActivity(new Intent(this, AppSettingsActivity.class)));
         help_btn.setOnClickListener(view -> startActivity(new Intent(this, HelpActivity.class)));
         share_wordpress_btn.setOnClickListener(view -> Toast.makeText(this, getString(R.string.under_dev), Toast.LENGTH_SHORT).show());
         wp_admin_btn.setOnClickListener(view -> Toast.makeText(this, getString(R.string.under_dev), Toast.LENGTH_SHORT).show());
+
+        getUserInfo(accessToken);
 
         //done logout
         log_out_btn.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +99,41 @@ public class MeWebsiteActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(v -> {
             finish();
         });
+
+    }
+    private void getUserInfo(String accessToken){
+        String url = "https://public-api.wordpress.com/rest/v1.1/me";
+        StringRequest fetchUserInfoRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+                response -> {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        displayName.setText(jsonResponse.getString("display_name"));
+                        accountName.setText(jsonResponse.getString("username"));
+                        String path = jsonResponse.getString("avatar_URL");
+                        Picasso.get()
+                                .load(path)
+                                .placeholder(R.drawable.blank_avatar)
+                                .error(R.drawable.blank_avatar)
+                                .fit()
+                                .into(avatar);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                error -> {
+                    VolleyLog.d("Volley", "Error" + error.getMessage());
+                    error.printStackTrace();
+                }
+        ){
+            @Override
+            public Map<String,String> getHeaders(){
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + accessToken);
+                return headers;
+            }
+        };
+        QueueManager.getInstance(this).addToRequestQueue(fetchUserInfoRequest);
     }
 }
-
